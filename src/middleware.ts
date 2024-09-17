9b22b907aae7de53de93e68992fe6f36fc5aec87
towelie <mail@towelie.dev>
2024-09-17 15:52:56 +0200

    added middleware to protect all pages (expecpt the regex in matcher) for unauthenticated users.
    We defined a dynamic callback url so after logging in, the user gets
    redirected to the page he tried to access before

---
+++ src/middleware.ts
@@ -0,0 +1,27 @@
+import { NextResponse } from "next/server"
+import { auth, BASE_PATH } from "./auth"
+
+export const config = {
+    matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"]
+}
+
+// this method gets called on requests,
+// that match the exported config
+export default auth((req) => {
+    const url = new URL(req.url)
+
+    // ignore for "home" page
+    if (!req.auth && url?.pathname !== "/") {
+
+        // redirect to login with dynamic callback url
+        return NextResponse.redirect(
+            new URL(
+                `${BASE_PATH}/signin?callbackUrl=${encodeURIComponent(
+                    url?.pathname
+                )}`,
+                req.url
+            )
+        )
+
+    }
+})
