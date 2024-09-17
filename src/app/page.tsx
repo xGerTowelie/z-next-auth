4f53504016ec4e5fc34953964555a4ad2b5fbc0f
towelie <mail@towelie.dev>
2024-09-17 16:08:52 +0200

    added server function to use authentication.
    we added the test for this inside the `page.tsx` since this is the only
    route we can access without the middleware redirecting to the signin
    page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,17 @@
 import AuthButton from "@/auth/AuthButton.server";
+import { getLoggedInUser } from "@/server/actions";
 
-export default function Home() {
+export default async function Home() {
+    // ask the server whats the current user
+    // we test this here, since this is the only
+    // route we can access without the middleware
+    // redirecting us to signin
+    const user = await getLoggedInUser()
     return (
         <div>
             <h1>Home</h1>
             <AuthButton />
+            <p>Current User: {user}</p>
         </div>
     )
 }
